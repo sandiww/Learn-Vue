bec88ee9019a3e022cd71059aee528cb2f94c5c8
Sandi Suryadi <sandis.pragma@gmail.com>
2020-07-08 12:17:53 +0700

    header and spacing

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,77 @@
 <template>
   <div id="app">
-      <h2>
-        What i do today ?
-      </h2>
-      <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="tugas">
-        <p>{{ tugasSaya.namaTugas }}</p>
+    <div class="container">
+      <div class="row header mb-5 p-3">
+        <div class="col-lg-12">
+          <h2>
+            What i do today ?
+          </h2>
+        </div>
       </div>
+      <div class="row content">
+        <div class="col-lg-12">
+          <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="tugas">
+            <p>{{ tugasSaya.namaTugas }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data: function() {
-    return {
-      tugas: [
-        {
-          id: 1,
-          namaTugas: 'Membersihkan Toilet'
-        }
-      ]
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        tugas: [{
+            id: 1,
+            namaTugas: 'Membersihkan Toilet',
+            isDone: false
+          },
+          {
+            id: 2,
+            namaTugas: 'Makan',
+            isDone: false
+          },
+          {
+            id: 3,
+            namaTugas: 'Ngoding',
+            isDone: false
+          }
+        ]
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav {
-  padding: 30px;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+
+  .header {
+    background: #42b983;
+  }
+
+  .header h2 {
+    margin: 0;
+  }
 </style>
